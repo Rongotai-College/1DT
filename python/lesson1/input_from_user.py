852f381fad9287e1c39df04a47e41e8d0c70a58c
Edsol <66800975+edsensanz@users.noreply.github.com>
2022-09-01 13:20:29 +1200

    Add files via upload

---
+++ python/lesson1/input_from_user.py
@@ -0,0 +1,27 @@
+# E M Petersen
+# User input
+
+# Taking input from the user as an integer
+# num = int(input("Enter a number:"))
+# add = num + 1
+
+# output
+# print(add)
+
+# Taking input from the user as float
+# num = float(input("Enter a number:"))
+# add = num + 1
+
+# output
+# print(add)
+
+# Taking input from the user as list
+# li = list(input("Enter a number of 5 digits: "))
+# print(li)
+
+# Taking input from the user as tuple
+num = tuple(input("Enter a number:"))
+print(num)
+
+
+
