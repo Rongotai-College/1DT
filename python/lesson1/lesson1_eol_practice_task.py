3abf57f619dabb846bf901ac0640549c22e9e00f
Edsol <66800975+edsensanz@users.noreply.github.com>
2022-09-01 13:23:01 +1200

    Add files via upload

---
+++ python/lesson1/lesson1_eol_practice_task.py
@@ -0,0 +1,43 @@
+# E M Petersen
+# Variables, labels, data types, output, comments
+
+# Variables
+name = "Eddie"
+height = 1.79
+address = "170 Coutts Street"
+phone = "049391234"
+hasAllergy = False
+
+# Summary printout
+print(name)
+print(height)
+print(address)
+print(phone)
+print(hasAllergy)
+
+# Clarify the Problem
+# Decide on the name variable
+fullName = "Eddie Petersen"
+firstName = "Eddie"
+lastName = "Petersen"
+title = "Mr"
+
+# define the height measurement
+# metres
+height = 1.79
+
+# define address
+# street number and name
+# address requires ONLY suburb
+address = "170 Coutts Street"
+
+
+
+# phone landline or mobile
+phone = "049391234"
+
+# Allergy - only defined as having one but not the detail
+hasAllergy = False
+
+
+
