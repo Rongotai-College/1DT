4ac65b3ed35168d7406bf4fdcd6ba1ddd23bfbbb
Edsol <66800975+edsensanz@users.noreply.github.com>
2022-09-06 10:19:31 +1200

    Add files via upload

---
+++ python/lesson2/user_input.py
@@ -0,0 +1,34 @@
+# E M Petersen
+# User input types
+
+# Taking input from the user
+# string = input()
+# Output
+# print(string)
+
+# taking input from the user
+# name = input("Enter your name")
+# Output
+# print(name)
+
+# taking input from the user
+# name = input("Enter your name: ")
+# Output
+# print("Hello,", name, ".")
+
+# Taking input from the user as integer
+# num = int(input("Enter a number: "))
+# add = num + 1
+# Output
+# print(add)
+
+# Taking input from the user as float
+# num = float(input("Enter a number: "))
+# add = num + 1
+# Output
+# print(add)
+
+# Taking input from the user as list
+# li = list(input("Enter a number: "))
+# Output
+# print(li)
